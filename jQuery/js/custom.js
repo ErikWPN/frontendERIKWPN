bd5bb1223a5cf6a145778d407aa94987bcb27d2d
ErikPC <erikwesley16@gmail.com>
2023-06-30 11:24:38 -0300

    modulo19

--- jQuery/js/custom.js
+++ jQuery/js/custom.js
@@ -40,3 +40,60 @@ $('featured-item a').on('click', function(event){
 
 })
 // console.log$($('h4').text());
+/*
+* Callback
+* Entendendo ações que começam ao termino de outra(sequencia!)
+ */
+    $('featured-item:nth(1)')
+        .hide(2000, function(){
+        // este é callback
+        console.log($(rhis).find('h4').text()+ 'Esgotato')
+    })
+    .show(2000, function(){
+        console.log($(rhis).find('h4').text()+ 'Em Estoque')
+    })
+
+    /*
+     * Animação
+     */
+    const duracao = 1000 // equivalente a 1 segundo
+    $('featured-item:nth(0)')
+        .hide(duracao)
+        .show(duracao)
+        .fadeOut(duracao)
+        .fadeIn(duracao)
+        .toggle(duracao)
+        .toggle(duracao)
+
+    $('#form-submit').on('click', function(e){
+
+        e.preventDefault()
+
+        if($('#email').val() !=''){
+            $('#email').animate({
+                opacity: "toggle",
+                top:"-50"
+            }, 500, function(){
+                console.log($(this).val())
+            })
+        }
+
+    });
+
+    /* 
+    * Ouvinte de eventos .nav-modal-open
+    */
+   $('nav-modal-open').on('click', function(){
+
+        e.preventDefault()
+
+        let elem = $(this).attr('rel')
+
+        $('.modal-body').html($('#'+elem).html())
+        $('.modal-header h5 w.modal-title').html($(this).text())
+
+        let myModal = new bootstrap.Modal($('#modalId'))
+
+        myModal.show()
+
+   })
